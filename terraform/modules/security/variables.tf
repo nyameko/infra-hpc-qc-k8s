7372bd117c364fca6c3fdd4657c5b7e3ac065512
Nyameko Lisa <connect@nyameko.com>
2026-08-29 21:17:25 +0200

    Security groups bricked.

--- terraform/modules/security/variables.tf
+++ terraform/modules/security/variables.tf
@@ -1,6 +1,5 @@
 variable "name_prefix" { type = string }
 variable "bootstrap_ssh_cidr" { type = string }
 variable "vpn_cidr" { type = string }
-variable "api_lb_cidr" { type = string }
 variable "mgmt_cidr" { type = string }
 variable "k8s_cidr" { type = string }
