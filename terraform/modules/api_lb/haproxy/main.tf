64363f8535df35963ee1d06970b550b9e04bea77
Nyameko Lisa <connect@nyameko.com>
2026-08-29 23:08:42 +0200

    Fixed bug where flavor_id was referencing flavor_name

--- terraform/modules/api_lb/haproxy/main.tf
+++ terraform/modules/api_lb/haproxy/main.tf
@@ -13,7 +13,7 @@ resource "openstack_networking_port_v2" "api_lb" {
 resource "openstack_compute_instance_v2" "api_lb" {
   name         = var.name
   image_id     = var.image_id
-  flavor_id    = var.flavor_id
+  flavor_name  = var.flavor_name
   key_pair     = var.key_pair
   config_drive = true
   user_data    = var.user_data
--- terraform/modules/api_lb/haproxy/variables.tf
+++ terraform/modules/api_lb/haproxy/variables.tf
@@ -4,9 +4,14 @@ variable "subnet_id" { type = string }
 variable "vip_address" { type = string }
 variable "security_group_ids" { type = list(string) }
 variable "image_id" { type = string }
-variable "flavor_id" { type = string }
+variable "flavor_name" { type = string }
 variable "key_pair" { type = string }
-variable "lister_port" {
+variable "backend_addresses" { type = list(string) }
+variable "backend_port" {
+  type    = number
+  default = 6443
+}
+variable "listener_port" {
   type    = number
   default = 6443
 }
