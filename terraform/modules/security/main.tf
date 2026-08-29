a0d7e21fadc62bb6a8b3ae084fa056b631f33c73
Nyameko Lisa <connect@nyameko.com>
2026-08-29 21:50:34 +0200

    Restricted access to different subnets in security groups.

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -12,7 +12,8 @@ locals {
 }
 
 resource "openstack_networking_secgroup_v2" "this" {
-  for_each    = local.groups
+  for_each = local.groups
+
   name        = "${var.name_prefix}-${each.key}"
   description = "${var.name_prefix} ${each.key}"
 }
@@ -37,36 +38,37 @@ resource "openstack_networking_secgroup_rule_v2" "edge_wireguard" {
   security_group_id = openstack_networking_secgroup_v2.this["edge"].id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "internal_all" {
-  for_each = {
-    edge             = "edge"
-    hermes           = "hermes-orchestrator"
-    slurm_controller = "slurm-controller"
-    slurm_login      = "slurm-login"
-    slurm_compute    = "slurm-compute"
-    api_lb           = "api-lb"
-    k8s_control      = "k8s-control-plane"
-    k8s_worker       = "k8s-worker"
-  }
+resource "openstack_networking_secgroup_rule_v2" "edge_ssh_vpn" {
   direction         = "ingress"
   ethertype         = "IPv4"
-  protocol          = null
-  remote_ip_prefix  = each.value == "edge" ? var.mgmt_cidr : var.mgmt_cidr
-  security_group_id = openstack_networking_secgroup_v2.this[each.value].id
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = var.vpn_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["edge"].id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "api_lb" {
+resource "openstack_networking_secgroup_rule_v2" "api_lb_ingress" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 6443
   port_range_max    = 6443
-  remote_ip_prefix  = var.mgmt_cidr
+  remote_ip_prefix  = var.vpn_cidr
   security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "k8s_api_vpn" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = var.vpn_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["k8s-control-plane"].id
+}
 
-resource "openstack_networking_secgroup_rule_v2" "k8s_api" {
+resource "openstack_networking_secgroup_rule_v2" "k8s_api_from_k8s" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -76,17 +78,27 @@ resource "openstack_networking_secgroup_rule_v2" "k8s_api" {
   security_group_id = openstack_networking_secgroup_v2.this["k8s-control-plane"].id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "k8s_api_k8s" {
+resource "openstack_networking_secgroup_rule_v2" "k8s_etcd" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 6443
-  port_range_max    = 6443
+  port_range_min    = 2379
+  port_range_max    = 2380
   remote_ip_prefix  = var.k8s_cidr
   security_group_id = openstack_networking_secgroup_v2.this["k8s-control-plane"].id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "k8s_kubelet" {
+resource "openstack_networking_secgroup_rule_v2" "k8s_kubelet_control_plane" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 10250
+  port_range_max    = 10250
+  remote_ip_prefix  = var.k8s_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["k8s-control-plane"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_kubelet_worker" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -96,12 +108,42 @@ resource "openstack_networking_secgroup_rule_v2" "k8s_kubelet" {
   security_group_id = openstack_networking_secgroup_v2.this["k8s-worker"].id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "slurm_compute_6817" {
+resource "openstack_networking_secgroup_rule_v2" "slurm_controller" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 6817
+  port_range_max    = 6817
+  remote_ip_prefix  = var.mgmt_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["slurm-controller"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "slurm_compute" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6818
   port_range_max    = 6818
   remote_ip_prefix  = var.mgmt_cidr
   security_group_id = openstack_networking_secgroup_v2.this["slurm-compute"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "slurm_login_ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = var.vpn_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["slurm-login"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "hermes_ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = var.vpn_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["hermes-orchestrator"].id
 }
