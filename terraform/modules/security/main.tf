42cd677d5012d2ee06453fa6934c1bb71b9bc986
Nyameko Lisa <connect@nyameko.com>
2026-09-13 23:55:23 +0200

    Removed duplicate rules.

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -48,6 +48,26 @@ resource "openstack_networking_secgroup_rule_v2" "edge_ssh_vpn" {
   security_group_id = openstack_networking_secgroup_v2.this["edge"].id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "api_lb_http_vpn" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = var.vpn_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "api_lb_https_vpn" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = var.vpn_cidr
+  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
+}
+
 resource "openstack_networking_secgroup_rule_v2" "ssh_from_mgmt" {
   for_each = setsubtract(local.groups, ["edge"])
 
@@ -119,73 +139,6 @@ resource "openstack_networking_secgroup_rule_v2" "api_lb_prometheus_from_k8s" {
   remote_ip_prefix  = var.k8s_cidr
   security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
 }
-
-resource "openstack_networking_secgroup_rule_v2" "api_lb_prometheus" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol  = "tcp"
-
-  port_range_min = 8404
-  port_range_max = 8404
-
-  remote_ip_prefix = var.k8s_cidr
-
-  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "api_lb_http_k8s" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol  = "tcp"
-
-  port_range_min = 80
-  port_range_max = 80
-
-  remote_ip_prefix = var.k8s_cidr
-
-  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "api_lb_https_k8s" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol  = "tcp"
-
-  port_range_min = 443
-  port_range_max = 443
-
-  remote_ip_prefix = var.k8s_cidr
-
-  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "api_lb_http_vpn" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol  = "tcp"
-
-  port_range_min = 80
-  port_range_max = 80
-
-  remote_ip_prefix = var.vpn_cidr
-
-  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "api_lb_https_vpn" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  protocol  = "tcp"
-
-  port_range_min = 443
-  port_range_max = 443
-
-  remote_ip_prefix = var.vpn_cidr
-
-  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
-}
-
-
 
 resource "openstack_networking_secgroup_rule_v2" "k8s_etcd" {
   direction         = "ingress"
