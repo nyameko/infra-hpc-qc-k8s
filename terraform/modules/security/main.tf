6cbc08713cc1cd828460c6bdc85c7415a892c9a8
Nyameko Lisa <connect@nyameko.com>
2026-09-13 23:42:09 +0200

    Added security group rules for HAProxy, and added first grafna dashboard.

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -120,6 +120,73 @@ resource "openstack_networking_secgroup_rule_v2" "api_lb_prometheus_from_k8s" {
   security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "api_lb_prometheus" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 8404
+  port_range_max = 8404
+
+  remote_ip_prefix = var.k8s_cidr
+
+  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "api_lb_http_k8s" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 80
+  port_range_max = 80
+
+  remote_ip_prefix = var.k8s_cidr
+
+  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "api_lb_https_k8s" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 443
+  port_range_max = 443
+
+  remote_ip_prefix = var.k8s_cidr
+
+  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "api_lb_http_vpn" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 80
+  port_range_max = 80
+
+  remote_ip_prefix = var.vpn_cidr
+
+  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "api_lb_https_vpn" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol  = "tcp"
+
+  port_range_min = 443
+  port_range_max = 443
+
+  remote_ip_prefix = var.vpn_cidr
+
+  security_group_id = openstack_networking_secgroup_v2.this["api-lb"].id
+}
+
+
+
 resource "openstack_networking_secgroup_rule_v2" "k8s_etcd" {
   direction         = "ingress"
   ethertype         = "IPv4"
